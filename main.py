00d441f45e7fcb5281953688a21ce49204874731
Benjamin Sykes <33205078+sykeben@users.noreply.github.com>
2018-04-12 10:34:46 -0400

    Updated

---
+++ main.py
@@ -0,0 +1,19 @@
+# MathStation Main Script
+# (C)2018 - Benjamin Sykes
+
+import cmd
+
+class MathShell (cmd.Cmd):
+    intro = 'Welcome to MathShell on MathStation.   Type help or ? to list commands.\n'
+    prompt = '[Math]$ '
+    file = None
+
+    # ----- Basic Math Commands -----
+    def do_add(self, ):
+
+def parse(arg):
+    'Convert a series of zero or more numbers to an argument tuple'
+    return tuple(map(int, arg.split()))
+
+if (__name__ == '__main__'):
+    MathShell().cmdloop()
